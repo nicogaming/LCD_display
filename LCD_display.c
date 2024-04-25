4134863db2b8cdcd2eb5debbf17cf389768f3361
NicoJBMortensen <61825874+nicogaming@users.noreply.github.com>
2024-04-25 12:27:41 +0200

    Update LCD_display.c
    tilføjer alle ændringer og er det færdig project uden buzzer funktion men med custom includes tilføjet

--- LCD_display.c
+++ LCD_display.c
@@ -10,6 +10,10 @@
 #include <i2c/smbus.h>
 #include <byteswap.h>
 
+// CUSTOM INCLUDES
+#include "lcd.h"
+#include "temp.h"
+
 #define MPC9808_BUS     "/dev/i2c-2"
 #define MPC9808_ADR     0x18
 #define TA_REG          0x05
@@ -21,93 +25,21 @@
 #define LCD_RGB_ADDRESS 0x62
 #define I2C_DEV "/dev/i2c-2"
 
-#ifndef I2C_FUNCTIONS_H
-#define I2C_FUNCTIONS_H7
-
-//creates a connection for the I2C display and backlight
+//creates a connection for the I2C display backlight and tempreader
 int lcd_fd;
 int lcd_bl_fd;
-
-//initializes communication with a I2C device on a specified I2C bus
-int i2c_init(char *bus, unsigned int address) {
-    int file = open(bus, O_RDWR);
-    if (file < 0) {
-        fprintf(stderr, "ERROR: opening %s - %s\n", bus, strerror(errno));
-        exit(1);
-    }
-
-    if (ioctl(file, I2C_SLAVE, address) == -1) {
-        fprintf(stderr, "ERROR: setting address %d on i2c bus %s with ioctl() - %s", address, bus, strerror(errno));
-        exit(1);
-    }
-    printf("i2c init\n");
-    return file;
-}
-
-#endif
-
-// Function to send a command to the LCD
-void lcdCommand(int fd, unsigned char command, int cb) {
-	unsigned char buf[2] = {cb, command};
-	buf[2] = command;
-    if(write(fd, buf, sizeof(buf)) != sizeof(buf)) {
-        perror("Failed to write to the I2C bus");
-    }
-    usleep(1000);
-}
-
-// Function to send data (character) to the LCD
-void lcdData(int fd, unsigned char data) {
-    unsigned char buf[2] = {0x40, data};
-    if(write(fd, buf, sizeof(buf)) != sizeof(buf)) {
-        perror("Failed to write to the I2C bus");
-    }
-    usleep(1000);
-}
-
-//reading the temperature from the tempreader
-float readTemperature(int file) {
-    int32_t reg32 = i2c_smbus_read_word_data(file, TA_REG);
-    if (reg32 < 0) {
-        fprintf(stderr, "ERROR: Read failed on i2c bus register %d - %s\n", TA_REG, strerror(errno));
-        exit(1);
-    }
-
-    // Convert to Celsius
-    reg32 = be16toh(reg32);  // Convert from big-endian to host byte order
-    float temperature = (float)((reg32 & 0x0FFF) / 16.0);
-    if (reg32 & 0x1000) {
-        temperature -= 256.0;  // For negative temperatures
-    }
-    return temperature;
-}
-
+int temp_fd;
 
 int main() {
-    int tempFile = i2c_init(MPC9808_BUS, MPC9808_ADR);
+
     // Open the I2C device
-    if((lcd_fd = open(I2C_DEV, O_RDWR)) < 0) {
-        perror("Failed to open the I2C device");
-        return 1;
-    }
+    temp_fd = init_temp(MPC9808_BUS, MPC9808_ADR);
 
-    if ((lcd_bl_fd = open(I2C_DEV, O_RDWR)) < 0) {
-    	perror("Failed to open the I2C device");
-	    return 1;
-    }
+    lcd_fd = init_lcd_fd(I2C_DEV, lcd_fd, LCD_ADDRESS);
 
-    // Set the I2C slave address
-    if(ioctl(lcd_fd, I2C_SLAVE, LCD_ADDRESS) < 0) {
-        perror("Failed to set I2C slave address");
-        close(lcd_fd);
-        return 1;
-    }
+    lcd_bl_fd = init_lcd_bl_fd(I2C_DEV, lcd_bl_fd, LCD_RGB_ADDRESS);
 
-    if (ioctl(lcd_bl_fd, I2C_SLAVE, LCD_RGB_ADDRESS) < 0) {
-    	perror("Failed to set I2C slave address");
-	    close(lcd_bl_fd);
-	    return 1;
-    }
+
 
     lcdCommand(lcd_fd, 0x01, 0x00);
 
@@ -124,7 +56,7 @@ int main() {
 
     while (1) {
         lcdCommand(lcd_fd, 0x01, 0x00); // Clear display
-        float temperature = readTemperature(tempFile);
+        float temperature = read_temperature(temp_fd, TA_REG);
 
         // Print a message to the LCD
         char tempStr[20];
@@ -134,9 +66,11 @@ int main() {
         } else if (temperature > 24) {
             //changes the backlight to red when the temp is over 24
             lcdCommand(lcd_bl_fd, 0x10, 0x8);
+
         } else {
             //changes the temp to green when the temp is between 20 and 24
             lcdCommand(lcd_bl_fd, 0x04, 0x8);
+
         }
 
         for (int i = 0; tempStr[i] != '\0'; i++) {
@@ -150,5 +84,6 @@ int main() {
     close(lcd_bl_fd);
 
     return 0;
+}
 }
 
